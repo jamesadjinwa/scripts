6b3e3f17f481c2b91dc2fb8d20e7e21b454ea185
jamesadjinwa <jaymee126@gmail.com>
2016-01-12 14:43:10 +0100

    Format the content of the resulting csv file

--- youtube-url.sh
+++ youtube-url.sh
@@ -7,8 +7,10 @@
 
 clear
 
+declare -rx SCRIPT=${0##*/} 			# SCRIPT is the name of this script
 declare -r WGET=`which wget`
 declare -r CUT=`which cut`
+declare -r AWK=`which awk`
 
 # Welcome message
 echo "-----------------------------------------------------------------------------"
@@ -29,6 +31,7 @@ read -p "Enter the url of the youtube playlist or channel: " url
 #fi
 
 # Check if its a youtube url
+function CheckYoutubeUrl {
 if [ `echo $url | grep ^https://www.youtube.com > /dev/null` -o `echo $url | grep ^http://www.youtube.com > /dev/null` ]
 then
 	echo "Good! This is a Youtube url"
@@ -45,8 +48,10 @@ else
 	echo "Sorry! This is not a youtube url";
 	exit
 fi
+}
 
 # Check if url is that of a youtube playlist 
+function CheckYoutubePlaylistUrl {
 string2test="playlists"		# Youtube's playlist url end with the string : "playlists"
 stringinurl=`echo "$url" | $CUT -d / -f 6`
 if [ "$string2test" != "$stringinurl" ]	# "playlist" is at the 6th position in the url
@@ -56,19 +61,30 @@ then
 else
 	echo "Good! This is a playlist url"
 fi
+}
 
 # Check if playlist exists
+function CheckYoutubeUrlConnection {
 echo "Connecting to $url ..."
 $WGET -O - $url > /dev/null
 case $? in
 	0 ) echo "Got it !";;
-	4 ) echo "Network failure";;
+	4 ) echo "Probably a network failure";; 
 	* ) echo "An error occured!"; exit
 esac
 
+}
+
+# TODO
+# Check if url is that of a youtube channel
+function CheckYoutubeChannelUrl {
+sleep 2
+}
+# Check if playlist exits
 
 # Extract urls in the youtube playlist and 
 # Create a csv file and write into it the extracted urls 
+function ExtractYoutubePlaylistLinks {
 echo "Extracting links in $url ... "
 # Check the number of links
 numberoflinks=`$WGET -qO - $url | grep -o '<h3 .*href=\"\/playlist.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]/    /' -e 's/["'"'"'].*$//' -e '/^$/ d' | sed -e 's/<h3 .*class=$//g' | sed '/^\s*$/d' | wc -l`
@@ -79,8 +95,26 @@ then
 else
 	echo "$numberoflinks links found !"
 fi
+}
+
+function WriteLinksToFile {
 username=`echo $url | $CUT -d / -f 5` 			# Etract the username. Will be part of the csv filename.
-`$WGET -qO - $url | grep -o '<h3 .*href=\"\/playlist.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | sed -e 's/<h3 .*class=$//g' | sed '/^\s*$/d' | sed -e "s/^/https:\/\/www.youtube.com\/user\/$username/" >$username"_playlist.csv"`
+#`$WGET -qO - $url | grep -o '<h3 .*href=\"\/playlist.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/<a .*href=['"'"'"]//' -e 's/["'"'"'].*$//' -e '/^$/ d' | sed -e 's/<h3 .*class=$//g' | sed '/^\s*$/d' | sed -e "s/^/https:\/\/www.youtube.com\/user\/$username/" >$username"_playlist.csv"`
+
+
+# Write name of link
+# Awk does the construction of csv file with "," as seperator
+`$WGET -qO - $url | grep -o '<h3 .*href=\"\/playlist.*>' | sed -e 's/<a /\n<a /g' | sed -e 's/title=/\ntitle=/g' | sed -e 's/href=['"'"'"]/\n/g' | sed -e 's/title=['"'"'"]//g' | sed -e 's/^<a //g' | sed -e 's/^<h3 //g' | sed -e 's/^class.*$//g' | sed -e 's/".*$//g' | sed '/^\s*$/d' | sed -e '/^\/playlist/! s/.*/"&"/g' | sed -e 's/^\/playlist/https:\/\/www.youtube.com&/' | $AWK '!/playlist/{if (x)print x;x="";}{x=(!x)?$0:x","$0;}END{print x;}' >$username"_playlist.csv"`
+
+# Insert header in csv file (Title, Link)
+`sed -i.bak 1i"Title,Link" $username"_playlist.csv"`
+}
+
+CheckYoutubeUrl 
+CheckYoutubePlaylistUrl
+CheckYoutubeUrlConnection 
+ExtractYoutubePlaylistLinks 
+WriteLinksToFile 
 
 exit
 # END
